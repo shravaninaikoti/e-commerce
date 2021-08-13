89b6881f69f0389180d4789ed59e6b56dce2ba38
shravaninaikoti <shravaninaikoti99@gmail.com>
2021-08-13 12:54:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?paheSize=50').subscribe((Response:any)=>{
+      console.log(Response);
+  },error=>{
+    console.log(error);
+    
+  }
+    );
+  }
+  
 }
